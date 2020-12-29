0bc3ab5d988b1fa48adff02137e11627e68c81c4
timotheemcdonald <timotheemcdonald@gmail.com>
2020-12-29 18:20:37 -0500

    added tinder cards component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="app">
 
      <Header />
-      {/* {Tinder Cards} */}
+     <TinderCards />
       {/* {Swipe Buttons} */}
     </div>
   );
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 function TinderCards(){
+
+    const [people, setPeople] = useState([
+        {
+            name: 'Test',
+            url: "",
+        }
+    ]);
+
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing: " + nameToDelete);
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + " left the screen!");
+    }
+
     return(
         <div className="tinderCards">
+           <div className="tinderCards__cardContainer">
+            {people.map( (person) => 
+                <TinderCard
+                className="swipe"
+                key={person.name}
+                preventSwipe={["up","down"]}
+                onSwipe={(dir) => swiped(dir, person.name)}
+                onCardLeftScreen={() => outOfFrame(person.name)}
+                >
 
+                <div
+                style={{backgroundImage: `url(${person.url})`}}
+                className="card">
+                    <h3>{person.name}</h3>
+                </div>
+
+                </TinderCard>
+            )}
+            </div>
         </div>
     )
 }
